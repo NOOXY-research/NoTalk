3c7dbdc6d1899fcd74656050707a737e75985ee8
BuildTools <magneticchen@gmail.com>
2019-01-16 16:37:35 +0800

    add metaupdate event

--- entry.js
+++ entry.js
@@ -6,21 +6,21 @@
 let NoTalk = require('./NoTalk');
 let fs = require('fs');
 
-function Service(Me, api) {
+function Service(Me, NoService) {
   // Your service entry point
   // Get the service socket of your service
-  let ss = api.Service.ServiceSocket;
+  let ss = NoService.Service.ServiceSocket;
   // BEWARE! To prevent callback error crash the system.
   // If you call an callback function which is not API provided. Such as setTimeout(callback, timeout).
-  // You need to wrap the callback funciton by api.SafeCallback.
-  // E.g. setTimeout(api.SafeCallback(callback), timeout)
-  let safec = api.SafeCallback;
+  // You need to wrap the callback funciton by NoService.SafeCallback.
+  // E.g. setTimeout(NoService.SafeCallback(callback), timeout)
+  let safec = NoService.SafeCallback;
   // Please save and manipulate your files in this directory
   let files_path = Me.FilesPath;
   // Your settings in manifest file.
   let settings = Me.Settings;
 
-  let notalk = new NoTalk(Me, api);
+  let notalk = new NoTalk(Me, NoService);
 
 
   // Your service entry point
@@ -30,11 +30,26 @@ function Service(Me, api) {
         console.log(err);
       }
       else {
-        ss.def('getMyMeta', (json, entityId, returnJSON)=> {
-          api.Authorization.Authby.Token(entityId, (err, valid)=> {
+        ss.on('connect', (entityId, callback)=> {
+          NoService.Authorization.Authby.Token(entityId, (err, valid)=> {
             if(valid) {
-              api.Service.Entity.getEntityOwner(entityId, (err, r)=>{
-                api.Authenticity.getUserID(r, (err, id)=>{
+              NoService.Service.Entity.getEntityOwner(entityId, (err, username)=>{
+                NoService.Service.Entity.addEntityToGroups(entityId, [username], (err)=> {
+                  callback(err);
+                });
+              });
+            }
+            else {
+              callback(false);
+            }
+          });
+        });
+
+        ss.def('getMyMeta', (json, entityId, returnJSON)=> {
+          NoService.Authorization.Authby.Token(entityId, (err, valid)=> {
+            if(valid) {
+              NoService.Service.Entity.getEntityOwner(entityId, (err, r)=>{
+                NoService.Authenticity.getUserID(r, (err, id)=>{
                   notalk.getUserMeta(id, (err, meta)=> {
                     meta.n = r;
                     returnJSON(false, meta);
@@ -49,15 +64,16 @@ function Service(Me, api) {
         });
 
         ss.def('updateMyMeta', (json, entityId, returnJSON)=> {
-          api.Authorization.Authby.Token(entityId, (err, valid)=> {
+          NoService.Authorization.Authby.Token(entityId, (err, valid)=> {
             if(valid) {
-              api.Service.Entity.getEntityOwner(entityId, (err, r)=>{
-                api.Authenticity.getUserID(r, (err, id)=>{
+              NoService.Service.Entity.getEntityOwner(entityId, (err, r)=>{
+                NoService.Authenticity.getUserID(r, (err, id)=>{
                   notalk.updateUserMeta(id, json, (err)=> {
                     if(err) {
                       returnJSON(false, {s:err});
                     }
                     else {
+                      ss.emitToGroups([r], 'MyMetaUpdated', json);
                       returnJSON(false, {s:'OK'});
                     }
                   });
